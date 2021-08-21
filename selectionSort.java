ff75d3a379294b0dfe186482f69aaa6f0feabefd
DeepicaCharugundla <89292374+DeepicaCharugundla@users.noreply.github.com>
2021-08-21 22:19:06 +0530

    Update selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -1,32 +1,12 @@
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int t = sc.nextInt();
-    while(t>0){
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++){
-            import java.util.*;
+package com.company;
 
-            Class GFG{
-                arr[i] = sc.nextInt();
 
-            }
+import java.util.Scanner;
 
-            selectionSort obj = new selectionSort();
-            obj.selectionSort1(arr,n);
-
-            for(int i=0;i<n;i++){
-                System.out.println(arr[i]+" ");
-            }
-            System.out.println();
-            t--;
-        }
-    }
-}
 class selectionSort
 {
-	int  select(int arr[], int i)
-	{
+    public static int  select(int arr[], int i)
+    {
         // code here such that selectionSort() sorts arr[]
         int min1 = arr[i];
         int index = i;
@@ -36,20 +16,36 @@ class selectionSort
                 index = j;
             }
         }
-        
+
         return index;
-	}
-	
-	void selectionSort1(int arr[], int n)
-	{
-	    //code here
-	    int num = 0;
-	    for(int i=0;i<n;i++){
-	        num = this.select(arr,i);
-	        int temp = arr[i];
-	        arr[i] = arr[num];
-	        arr[num] = temp;
-	    }
-	    
-	}
+    }
+
+    public static void selectionSort1(int arr[], int n)
+    {
+        //code here
+        int num = 0;
+        for(int i=0;i<n;i++){
+            num = select(arr,i);
+            int temp = arr[i];
+            arr[i] = arr[num];
+            arr[num] = temp;
+        }
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        selectionSort1(arr,n);
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
 }
+
