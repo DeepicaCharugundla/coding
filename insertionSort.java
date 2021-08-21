7977044ee0e0c871740221822570c698bf014377
DeepicaCharugundla <89292374+DeepicaCharugundla@users.noreply.github.com>
2021-08-21 18:07:37 +0530

    Create insertionSort.java

---
+++ insertionSort.java
@@ -0,0 +1,48 @@
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while(t>0){
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            import java.util.*;
+
+            Class GFG{
+                arr[i] = sc.nextInt();
+
+            }
+
+            insertionSort obj = new insertionSort();
+            obj.insertionSort1(arr,n);
+
+            for(int i=0;i<n;i++){
+                System.out.println(arr[i]+" ");
+            }
+            System.out.println();
+            t--;
+        }
+    }
+}
+class insertionSort
+{
+  static void insert(int arr[],int i)
+  {
+       // Your code here
+       int key = arr[i];
+       int j = i-1;
+       
+       while(j>=0 && arr[j] > key){
+           arr[j+1] = arr[j];
+           j = j-1;
+       }
+       arr[j+1] = key;
+  }
+  //Function to sort the array using insertion sort algorithm.
+  public void insertionSort1(int arr[], int n)
+  {
+      //code here
+      for(int i = 1; i< n;i++){
+          insert(arr,i);
+      }
+  }
+}
